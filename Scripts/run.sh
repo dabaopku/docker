ad1941eac0c8f5f8132d2a894c180ec82426580a
guocong@kuaishou <guocong@kuaishou.com>
2018-04-13 20:36:42 +0800

    run

--- Scripts/run.sh
+++ Scripts/run.sh
@@ -4,4 +4,4 @@ docker run --rm -d -p 3306:3306 --name mysql dabaopku/docker:Mysql
 
 docker run --rm -d -p 27017:27017 --name mongo dabaopku/docker:Mongo 
 
-docker run -it --rm  -p 8081:80 --link mongo:mongo rock
+docker run --rm -d -p 8081:80 --name rockMongo --link mongo:mongo dabaopku/docker:RockMongo
