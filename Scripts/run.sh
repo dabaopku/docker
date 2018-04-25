8e07c215774735c683235002c305f7749d6377f1
guocong@kuaishou <guocong@kuaishou.com>
2018-04-25 11:43:36 +0800

    scripts

--- Scripts/run.sh
+++ Scripts/run.sh
@@ -1,7 +1,17 @@
 #! /bin/bash
 
-docker run --rm -d -p 3306:3306 --name mysql dabaopku/docker:Mysql 
+create_mysql() {
+    docker run --rm -d -p 3306:3306 --name mysql dabaopku/docker:Mysql
+}
 
-docker run --rm -d -p 27017:27017 --name mongo dabaopku/docker:Mongo 
+create_mongo() {
+    docker run --rm -d -p 27017:27017 -v mongo:/data/db --name mongo dabaopku/docker:Mongo 
+}
 
-docker run --rm -d -p 8081:80 --name rockMongo --link mongo:mongo dabaopku/docker:RockMongo
+create_rockmongo() {
+    docker run --rm -d -p 8081:80 --name rockMongo --link mongo:mongo dabaopku/docker:RockMongo
+}
+
+copy_volume() {
+    docker run --rm -it -v $1:/from -v $2:/to alpine ash -c "cd /from; cp -av . /to"
+}
